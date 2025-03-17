9ac090dc4d3ef651ed9ebfd9b6c22d047dc92c85
Bunbn <75515757+Bunbn@users.noreply.github.com>
2025-03-16 23:26:29 -0700

    Update heading_task.py

--- code/heading_task.py
+++ code/heading_task.py
@@ -3,9 +3,31 @@ import cotask
 import task_share
 
 class HeadingTask:
+    """
+    A class to handle reading the heading from an IMU and updating shared variables.
 
+    Attributes
+    ----------
+    imu : object
+        The IMU sensor instance used for reading heading data.
+    S0_init : int
+        The initialization state value.
+    S1_read : int
+        The read state value.
+    state : int
+        The current state value.
+    target_heading : int
+        The desired target heading value.
+    """
     def __init__(self, imu):
+        """
+        Initializes the HeadingTask object and sets up the IMU sensor.
 
+        Parameters
+        ----------
+        imu : object
+            The IMU sensor instance used to retrieve heading data.
+        """
         self.imu = imu
 
         self.S0_init = 0
@@ -15,7 +37,23 @@ class HeadingTask:
 
 
     def get_heading(self, shares):
+        """
+        The generator that implements the finite state machine for the Heading task.
 
+        State 0 initializes the IMU sensor to heading mode and transitions to state 1. 
+        State 1 reads the heading from the IMU, adjusts it within the 0-360° range, 
+        and updates the shared variable.
+
+        Parameters
+        ----------
+        shares : task_share.Share
+            A shared variable to store the heading data.
+
+        Yield
+        ------
+        int
+            The value of the current state within the machine.
+        """
         heading = shares
 
         while True:
